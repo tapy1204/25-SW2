78b944887a505a68c6d134175e08ad0915bd94ac
tapy1204 <tapy1204@kookmin.ac.kr>
2025-11-03 18:29:36 +0900

    Add files via upload

---
+++ 13C02_1.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+#define PIN_SERVO 10
+
+#define _DUTY_MIN 500
+#define _DUTY_MAX 1550
+#define _SERVO_SPEED 0.3
+#define MOVE_TIME 300000
+
+Servo myservo;
+
+unsigned long startTime;
+float currentDuty = _DUTY_MIN;
+
+void setup(){
+  myservo.attach(PIN_SERVO);
+  myservo.writeMicroseconds(_DUTY_MIN);
+  startTime = millis();
+  Serial.begin(52800);
+}
+
+void loop(){
+  unsigned long elapsed = millis() - startTime;
+
+  if(elapsed <= MOVE_TIME){
+    float progress = (float)elapsed / MOVE_TIME;
+
+    currentDuty = _DUTY_MIN + progress * (_DUTY_MAX - _DUTY_MIN);
+
+    myservo.writeMicroseconds(currentDuty);
+  }
+  else{
+    myservo.writeMicroseconds(_DUTY_MAX);
+  }
+  
+  delay(20);
+}
